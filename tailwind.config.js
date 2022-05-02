91b705cf326e513a57d917aa9865c0b384bdc0df
thisisamondi <67878188+thisisamondi@users.noreply.github.com>
2022-05-02 17:07:56 +0200

    add homecomponent, tailwind and custom css

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  content: [   
+    "./pages/**/*.{js,ts,jsx,tsx}",   
+    "./components/**/*.{js,ts,jsx,tsx}",  
+     ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'green': '#32533D',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  }
+}
