a1654bbe5c2d90f39c8a81fe789da51d91f46dae
Maddy <maddyhedman@hotmail.com>
2022-05-31 19:47:49 +0200

    added some css

--- components/MidwifeComponent.jsx
+++ components/MidwifeComponent.jsx
@@ -1,23 +1,33 @@
 import Container from "./Container";
 
 const MidwifeComponent = ({ midwife }) => {
+  console.log(midwife);
   return (
-    <div className="">
-      <h1 className="font-serif text-purple text-center">{midwife.title}</h1>
-      <div className="grid grid-cols-3 gap-x-20 gap-y-10 py-20 px-10 ">
-        {midwife &&
-          midwife?.barnmorska?.map((midwife, index) => (
-            <div key={index} className="midwife-container">
-              <h1 className="font-serif  text-center">{midwife.fields.name}</h1>
-              <p className="font-serif text-center">
-                {" "}
-                {midwife.fields.website}
-              </p>
-              <p className="font-serif  text-center"> {midwife.fields.email}</p>
-            </div>
-          ))}
+    <Container>
+      <div className="">
+        <h1 className="font-serif text-purple text-center text-5xl pt-10">
+          {midwife.title}
+        </h1>
+        <div className="grid lg:grid-cols-3 gap-x-20 gap-y-10 py-20  md:grid-cols-2 ">
+          {midwife &&
+            midwife?.barnmorska?.map((midwife, index) => (
+              <div key={index} className="midwife-container h-72">
+                <h1 className="font-serif  text-center">
+                  {midwife.fields.name}
+                </h1>
+                <p className="font-serif text-center">
+                  {" "}
+                  {midwife.fields.website}
+                </p>
+                <p className="font-serif  text-center">
+                  {" "}
+                  {midwife.fields.email}
+                </p>
+              </div>
+            ))}
+        </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
