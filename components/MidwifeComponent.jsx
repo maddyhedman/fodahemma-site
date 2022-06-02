740a20383163d107de620481dda2d4700f50f966
thisisamondi <67878188+thisisamondi@users.noreply.github.com>
2022-06-02 11:00:50 +0200

    padding changes in midwife component

--- components/MidwifeComponent.jsx
+++ components/MidwifeComponent.jsx
@@ -1,4 +1,6 @@
 import Container from "./Container";
+import Image from "next/image";
+import purple from "../public/images/purple.png"
 
 const MidwifeComponent = ({ midwife }) => {
   return (
@@ -10,10 +12,11 @@ const MidwifeComponent = ({ midwife }) => {
         <div className="grid lg:grid-cols-3 gap-x-5 gap-y-5 py-20  md:grid-cols-2 mb-36">
           {midwife &&
             midwife?.barnmorska?.map((midwife, index) => (
-              <div key={index} className="midwife-container h-72 px-10">
-                <h1 className="font-serif pt-20 text-2xl">
+              <div key={index} className={"midwife-container h-80 pt-20 px-10"}>
+                <h1 className="font-serif text-2xl">
                   {midwife.fields.name}
                 </h1>
+        
                 {midwife.fields.phone && (
                   <p className="font-serif"> +{midwife.fields.phone}</p>
                 )}
