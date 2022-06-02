aebedfadbd2d92a248bc29b14971a7b47d76d644
Maddy <maddyhedman@hotmail.com>
2022-06-02 20:46:35 +0200

    erades font-serif

--- components/MidwifeComponent.jsx
+++ components/MidwifeComponent.jsx
@@ -4,7 +4,6 @@ import { TbWorld } from "react-icons/tb";
 import { TbMail } from "react-icons/tb";
 import { MdOutlinePlace } from "react-icons/md";
 
-
 const MidwifeComponent = ({ midwife }) => {
   return (
     <Container>
@@ -15,7 +14,6 @@ const MidwifeComponent = ({ midwife }) => {
         <div className="grid xl:grid-cols-3 md:grid-cols-2 gap-x-5 gap-y-5  md:py-20 py-5 md:mb-36  ">
           {midwife &&
             midwife?.barnmorska?.map((midwife, index) => (
-
               <div
                 key={index}
                 className="midwife-container flex flex-col items-center h-80 md:w-84 md:px-20 justify-center"
@@ -49,7 +47,7 @@ const MidwifeComponent = ({ midwife }) => {
                   </p>
                 )}
                 {midwife.fields.county && (
-                  <p className="font-serif flex items-center gap-2 ">
+                  <p className="flex items-center gap-2 ">
                     <MdOutlinePlace />
                     {midwife.fields.county}
                   </p>
