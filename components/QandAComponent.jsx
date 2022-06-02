65913b4df1c7461a6dbcddcb6428f063164f7610
thisisamondi <67878188+thisisamondi@users.noreply.github.com>
2022-06-02 17:39:26 +0200

    add style changes

--- components/QandAComponent.jsx
+++ components/QandAComponent.jsx
@@ -1,20 +1,21 @@
 import Container from "./Container";
 
 const QandAComponent = ({ qanda }) => {
+
   return (
     <div className="bg-green h-screen py-10">
       <Container>
         <h1 className="font-serif text-6xl text-white text-center">
-          {qanda.title}
+          {qanda?.title}
         </h1>
         <div className="grid grid-cols-2 gap-x-20 gap-y-10 py-20">
           {qanda &&
             qanda?.qAndA?.map((qanda, index) => (
               <div key={index}>
                 <h1 className="font-serif text-3xl text-white">
-                  {qanda.fields.question}
+                  {qanda?.fields?.question}
                 </h1>
-                <p className="text-[#D1C2B5] mt-2">{qanda.fields.answer}</p>
+                <p className="text-[#D1C2B5] mt-2">{qanda?.fields?.answer}</p>
               </div>
             ))}
         </div>
