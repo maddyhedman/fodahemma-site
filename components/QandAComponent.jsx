bd88e638560b4800f5d02a40766a6a6317b64214
thisisamondi <67878188+thisisamondi@users.noreply.github.com>
2022-06-02 20:56:00 +0200

    add responsive style

--- components/QandAComponent.jsx
+++ components/QandAComponent.jsx
@@ -3,19 +3,19 @@ import Container from "./Container";
 const QandAComponent = ({ qanda }) => {
 
   return (
-    <div className="bg-green h-screen py-10">
+    <div className="bg-green md:pt-10 pb-5">
       <Container>
-        <h1 className="font-serif text-6xl text-white text-center">
-          {qanda?.title}
+        <h1 className="font-serif md:text-5xl text-3xl text-white text-center">
+          {qanda.title}
         </h1>
-        <div className="grid md:grid-cols-2 gap-x-20 gap-y-10 py-20">
+        <div className="grid md:grid-cols-2 gap-x-20 gap-y-10 pt-10">
           {qanda &&
             qanda?.qAndA?.map((qanda, index) => (
               <div key={index}>
                 <h1 className="font-serif text-3xl text-white">
                   {qanda?.fields?.question}
                 </h1>
-                <p className="text-[#D1C2B5] mt-2">{qanda?.fields?.answer}</p>
+                <p className="text-[#D1C2B5] mt-2">{qanda.fields?.answer}</p>
               </div>
             ))}
         </div>
