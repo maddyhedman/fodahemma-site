851980881a39c8767556bc1052f98118fedef623
thisisamondi <67878188+thisisamondi@users.noreply.github.com>
2022-06-02 21:16:12 +0200

    add responsive style

--- components/QandAComponent.jsx
+++ components/QandAComponent.jsx
@@ -8,12 +8,12 @@ const QandAComponent = ({ qanda }) => {
         <h1 className="font-serif md:text-5xl text-3xl text-white text-center">
           {qanda.title}
         </h1>
-        <div className="grid md:grid-cols-2 gap-x-20 gap-y-10 pt-10">
+        <div className="grid md:grid-cols-2 gap-x-20 md:gap-y-10 pt-10">
           {qanda &&
             qanda?.qAndA?.map((qanda, index) => (
               <div key={index}>
                 <h1 className="font-serif text-3xl text-white">
-                  {qanda?.fields?.question}
+                  {qanda?.fields?.question} 
                 </h1>
                 <p className="text-[#D1C2B5] mt-2">{qanda.fields?.answer}</p>
               </div>
